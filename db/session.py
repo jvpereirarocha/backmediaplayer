df51cea9a1da2f71da9a9a77f990f75524831bc2
Joao Victor <jvpereirarocha@gmail.com>
2024-10-09 13:42:33 -0300

    feat: changing db configuration

--- db/session.py
+++ db/session.py
@@ -1,5 +1,6 @@
-from sqlalchemy.orm import registry, Mapped, mapped_column
-from sqlalchemy import Index
+from sqlalchemy.orm import registry, Mapped, mapped_column, sessionmaker, scoped_session
+from sqlalchemy import Index, create_engine
+from os import getenv
 
 
 mapped_registry = registry()
@@ -7,10 +8,29 @@ mapped_registry = registry()
 
 @mapped_registry.mapped_as_dataclass
 class Media:
+    __tablename__ = "media"
+
     media_id: Mapped[int] = mapped_column(init=False, primary_key=True, autoincrement=True)
     title: Mapped[str]
     url: Mapped[str] = mapped_column(unique=True)
 
     __table_args__ = (
-        Index('title_url_idx', 'title', 'url')
+        Index('title_url_idx', 'title', 'url'),
+        Index('title_idx', 'title'),
+        Index('url_idx', 'url'),
     )
+
+engine = None
+try:
+    engine = create_engine(
+        url=getenv("DATABASE_URI")
+    )
+except Exception:
+    raise ValueError("Database URI incorrect")
+
+
+Session = scoped_session(sessionmaker(bind=engine))
+
+
+def close_db_session():
+    Session.remove()
